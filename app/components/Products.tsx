31d7329e53b54520a062af58e48a4f9f2ac78ddb
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-04-30 22:28:46 -0500

    new file price formater, and product type

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,11 +1,14 @@
 // Render each product
 import Image from "next/image"
+import formatPrice from "@/util/PriceFormat"
+import { ProductType } from "@/types/ProductType"
 
-export default function Product({name, image, price}) {
+export default function Product({ name, image, price }: ProductType) {
 	return (
 		<div>
-            <Image src={image} alt={name} width={400} height={400} />
+			<Image src={image} alt={name} width={400} height={400} />
 			<h1>{name}</h1>
+			{price !== null ? formatPrice(price) : "N/A"}
 		</div>
 	)
 }
