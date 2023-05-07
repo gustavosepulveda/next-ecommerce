7d1b3d2adff28dd8342690375f59f4230bc659db
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-05-06 20:59:58 -0500

    started styling on products page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,18 +1,31 @@
 import Image from "next/image"
 import { SearchParamTypes } from "@/types/SearchParamTypes"
+import formatPrice from "@/util/PriceFormat"
 
 export default async function Product({ searchParams }: SearchParamTypes) {
 	return (
 		<div className="flex justify-between gap-24 p-12 text-gray-700">
+			{/* imaged of product */}
 			<Image
 				src={searchParams.image}
 				alt={searchParams.name}
 				width={600}
 				height={600}
 			/>
-			<div>
-				<h1>{searchParams.name}</h1>
-				<p>{searchParams.description}</p>
+			<div className="font-medium text-gray-700">
+				{/* name, description, Metadata/features of product */}
+				<h1 className="text-2xl font-medium py-2">{searchParams.name}</h1>
+				<p className="py-2">{searchParams.description}</p>
+				<p className="py-2">{searchParams.features}</p>
+				<div className="flex gap-2">
+					<p className="font bold text-teal-700">
+						{/* shows price of product */}
+						{searchParams.unit_amount && formatPrice(searchParams.unit_amount)}
+					</p>
+				</div>
+				<button className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700">
+					Add to Cart
+				</button>
 			</div>
 		</div>
 	)
