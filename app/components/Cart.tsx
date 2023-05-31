edf65428637b1023d8951252bc78f86480e46572
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-05-30 21:22:57 -0500

    added framer motion to cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -22,13 +22,14 @@ export default function Cart() {
 			onClick={() => cartStore.toggleCart()}
 			className="fixed w-full h-screen left-0 top-0 bg-black/25"
 		>
-			<div
+			<motion.div
+				layout
 				onClick={(e) => e.stopPropagation()}
 				className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700"
 			>
 				<h1>Heres your shopping list</h1>
 				{cartStore.cart.map((item) => (
-					<div className="flex py-4 gap-4">
+					<motion.div layout key={item.id} className="flex py-4 gap-4">
 						<Image
 							className="rounded-md h-24"
 							src={item.image}
@@ -36,7 +37,7 @@ export default function Cart() {
 							width={120}
 							height={120}
 						/>
-						<div>
+						<motion.div layout>
 							<h2>{item.name}</h2>
 							{/* Update quantity of a product */}
 							<div className="flex gap-2">
@@ -72,16 +73,19 @@ export default function Cart() {
 							<p className="text-sm">
 								{item.unit_amount && formatPrice(item.unit_amount)}
 							</p>
-						</div>
-					</div>
+						</motion.div>
+					</motion.div>
 				))}
 				{/* Check out and total */}
-				<p>Total: {formatPrice(totalPrice)}</p>
 				{cartStore.cart.length > 0 && (
-					<button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
-						Checkout
-					</button>
-				)}
+					<motion.div layout>
+						<p>Total: {formatPrice(totalPrice)}</p>
+
+						<button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
+							Checkout
+						</button>
+					</motion.div>
+				)} 
 				<AnimatePresence>
 					{!cartStore.cart.length && (
 						<motion.div
@@ -95,7 +99,7 @@ export default function Cart() {
 						</motion.div>
 					)}
 				</AnimatePresence>
-			</div>
+			</motion.div>
 		</motion.div>
 	)
 }
