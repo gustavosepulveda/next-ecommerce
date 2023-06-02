5042894b1ecefbaf12e792a28bfb0c0ce2cb25bf
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-06-01 22:50:28 -0500

    Checkout Component created

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,13 +5,14 @@ import AddCart from "./AddCart"
 
 export default async function Product({ searchParams }: SearchParamTypes) {
 	return (
-		<div className="flex justify-between gap-24 p-12 text-gray-700">
+		<div className="flex flex-col 2x1:flex-row items-center lg:flex justify-between gap-24 text-gray-700">
 			{/* imaged of product */}
 			<Image
 				src={searchParams.image}
 				alt={searchParams.name}
 				width={600}
 				height={600}
+				className="w-full"
 			/>
 			<div className="font-medium text-gray-700">
 				{/* name, description, Metadata/features of product */}
