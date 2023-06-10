1e7df4b750ad04bbcf7aa64981b8e5f0b9375f23
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-06-10 16:06:17 -0500

    added  Order model to prisma schema

--- pages/api/create-payment-intent.ts
+++ pages/api/create-payment-intent.ts
@@ -3,10 +3,13 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { authOptions } from "./auth/[...nextauth]"
 import { getServerSession } from "next-auth"
 import { AddCartType } from "@/types/AddCartType"
+import { PrismaClient } from "@prisma/client"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 	apiVersion: "2022-11-15",
 })
+
+const prisma = new PrismaClient()
 
 const calculateorderAmount = (items: AddCartType[]) => {
 	const totalPrice = items.reduce((acc, item) => {
@@ -40,11 +43,31 @@ export default async function handler(
 				name: item.name,
 				description: item.description,
 				unit_amount: item.unit_amount,
+				image: item.image,
 				quantity: item.quantity,
 			})),
 		},
 	}
 
-	res.status(200).json({ userSession })
+	//Check if the order payment intent exsits just update the order
+	if (payment_intent_id) {
+		const current_intent = await stripe.paymentIntents.retrieve(
+			payment_intent_id
+		)
+	} else {
+		//Creat a new order with prisma
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: calculateorderAmount(items),
+			currency: "usd",
+			automatic_payment_methods: { enabled: true },
+		})
+		orderData.payment_intentID = paymentIntent.id
+		const newOrder = await prisma.order.create({
+			data: orderData,
+		})
+	}
+
+	res.status(200).json({ message: "done" })
 	return
+	//Data necessary for the order
 }
