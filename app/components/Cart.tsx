46717c2159315de7b22d85bacfea05f5845e6755
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-07-04 12:17:06 -0500

    finished CheckoutForm

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -29,12 +29,23 @@ export default function Cart() {
 				onClick={(e) => e.stopPropagation()}
 				className="bg-white absolute right-0 top-0 h-screen p-12 overflow-y-scroll text-gray-700 w-full lg:w-2/5"
 			>
-				<button
-					onClick={() => cartStore.toggleCart()}
-					className="text-sm font-bold pb-12"
-				>
-					Back to store
-				</button>
+				{cartStore.onCheckout === "cart" && (
+					<button
+						onClick={() => cartStore.toggleCart()}
+						className="text-sm font-bold pb-12"
+					>
+						Back to store
+					</button>
+				)}
+				{cartStore.onCheckout === "checkout" && (
+					<button
+						onClick={() => cartStore.setCheckout("cart")}
+						className="text-sm font-bold pb-12"
+					>
+						Back to Cart
+					</button>
+				)}
+
 				{cartStore.onCheckout === "cart" && (
 					<>
 						{cartStore.cart.map((item) => (
@@ -88,7 +99,7 @@ export default function Cart() {
 					</>
 				)}
 				{/* Check out and total */}
-				{cartStore.cart.length > 0 && (
+				{cartStore.cart.length > 0 && cartStore.onCheckout === "cart" ? (
 					<motion.div layout>
 						<p>Total: {formatPrice(totalPrice)}</p>
 
@@ -99,7 +110,7 @@ export default function Cart() {
 							Checkout
 						</button>
 					</motion.div>
-				)}
+				) : null}
 				{/* Checkout Form */}
 				{cartStore.onCheckout === "checkout" && <Checkout />}
 				<AnimatePresence>
